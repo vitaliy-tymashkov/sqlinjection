57f7f99e4062d59691cf7feeba02947aabe34ff2
Vitalii Tymashkov <vitalii.tymashkov@neoplm.com>
2025-04-22 13:57:28 +0200

    Add logging of SQL for unsafe JDBC method

--- src/main/java/com/okta/blog/sqlinjection/repository/jdbc/EmployeeRepositoryJdbcUnSafe.java
+++ src/main/java/com/okta/blog/sqlinjection/repository/jdbc/EmployeeRepositoryJdbcUnSafe.java
@@ -1,15 +1,22 @@
 package com.okta.blog.sqlinjection.repository.jdbc;
 
+import com.okta.blog.sqlinjection.controller.EmployeeController;
 import com.okta.blog.sqlinjection.domain.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class EmployeeRepositoryJdbcUnSafe extends EmployeeRepositoryJdbc {
+    private static final Logger log = LoggerFactory.getLogger(EmployeeRepositoryJdbcUnSafe.class);
 
     @Override
     public List<Employee> filterByUsername(String name) {
+        String query = "select * from employee where name ='" + name + "'";
+        log.info("SQL query is: " + query);
+
         return jdbcTemplate.query("select * from employee where name ='" + name + "'", new EmployeeRowMapper());
     }
 
